4eece2eb43a543a3a55175876b49469671b7df80
Sergey Ryzhikov <sergey-inform@ya.ru>
2016-07-16 03:05:54 +0300

    temp

--- hist2.py
+++ hist2.py
@@ -19,9 +19,11 @@ import sys
 import os
 import signal
 import argparse
-from itertools import cycle # cycle facecolors
+from itertools import cycle  # cycle facecolors
 
-CHAN_COL_IDX = 1
+import numpy as np
+
+CHAN_COL_IDX = 1  # if None, no channel filtering at all.
 DATA_COL_IDX = 2
 
 
@@ -33,16 +35,28 @@ def sigint_handler(signal, frame):
         print('You pressed Ctrl+C!')
         sys.exit(0)
 
-def parse_input(_file, chans=None ):
 
+def parse_infile(_file, chans=None ):
+	"""
+	Parses a file with data records.
+	
+	chans:
+		a list of channel names; if specified, skip channels not in list
+	
+	Returns a dict {channel_0: values_0, ... channel_N: values_N}
+	"""
 	lineno = 0
-	ret = {}  # {chan: np.array, ...}
+	ret = {}  
+	
 	for line in _file:
 		lineno += 1
 
 		fields = line.split()
+		
 		try:
-			chan = fields[CHAN_COL_IDX]
+			chan = fields[CHAN_COL_IDX] \
+				if CHAN_COL_IDX is not None \
+				else None
 			val = float( fields[DATA_COL_IDX] )
 		
 		except IndexError as e:
@@ -52,28 +66,26 @@ def parse_input(_file, chans=None ):
 		except ValueError as e:
 			logging.error('%s , line: %d' % (e, lineno) )
 			raise
-		
+
 		if chans and chan not in chans:
 			continue  # skip non-listed channels
 		
 		try:
 			ret[chan].append(val)
 
-		except KeyError:
+		except KeyError:  # haven't seen this channel before
 			ret[chan] = []
 			ret[chan].append(val)
-
 	return ret
 
+
+#TODO: plot( data={trig: values, trig2: values2...
 
 def plot(data, outprefix=None, bins=None, histopts={}):
 	import matplotlib.pyplot as plt
 
 	defaults = dict(
-		alpha=0.75,
-		range=(0,4000),
-		normed=True, # replace with density=True in future matplotlib versions 
-		histtype='step',
+		
 		)
 	
 	# set default values for missing options:
@@ -126,18 +138,16 @@ def plot(data, outprefix=None, bins=None, histopts={}):
 	
 
 def freedman_bin_width(data):
-	"""Return the optimal an optimal number of bins using the Freedman-Diaconis rule
+	"""Return the optimal number of bins using the Freedman-Diaconis rule
 
 	The Freedman-Diaconis rule is a normal reference rule like Scott's
 	rule, but uses rank-based statistics for results which are more robust
 	to deviations from a normal distribution.
-	width : float
-	optimal bin width using the Freedman-Diaconis rule
 
 	
 	http://astropy.readthedocs.io/en/latest/_modules/astropy/stats/histogram.html#freedman_bin_width
 	"""
-	import numpy as np
+	
 	data = np.asarray(data)
 	if data.ndim != 1:
 		raise ValueError("data should be one-dimensional")
@@ -153,101 +163,197 @@ def freedman_bin_width(data):
 	dmin, dmax = data.min(), data.max()
 	nbins = max(1, np.ceil((dmax - dmin) / dx))
 	
-	return nbins
+	return nbins *2
 	
 	
-
+	
 def main():
 	global CHAN_COL_IDX
 	global DATA_COL_IDX
 	
-	signal.signal(signal.SIGINT, sigint_handler)
+	signal.signal(signal.SIGINT, sigint_handler) # catch Ctrl-C
 	
 	parser = argparse.ArgumentParser(description=__doc__,
 			formatter_class=argparse.RawTextHelpFormatter)
 			
-	parser.add_argument( 'infiles', type=argparse.FileType('r'),
+	parser.add_argument( 'infiles',
+			type=argparse.FileType('r'),
 			nargs='*',
 			default=[sys.stdin],
-			help='data files')
-
-	parser.add_argument('-r', '--range', type=str,
-			metavar='N:M',
-			default='0:2000',
-			help='a range of histogram values')
-	
-	parser.add_argument('--normalize', action='store_true',
-			help="normalize histograms (show density)")
-	
-	parser.add_argument('-b', '--bins', type=int, default=None,
-			help='a number of bins in the histogram (default: optimize)')
-			
-	parser.add_argument('-c','--chan', type=str, 
+			help='the files with data records'
+			)
+	parser.add_argument( '-c','--chan',
+			type=str, 
 			metavar = 'LIST',
-			help='a list of channel names, separated by commas')
-	
-	parser.add_argument('--chan-col', type=str, 
+			help='get data only for specified channel names'
+				' (separated by commas)'
+			)
+	parser.add_argument( '--normalize',
+			action='store_true',
+			help="normalize histograms (plot density)"
+			)
+	parser.add_argument('-r', '--range',
+			type=str,
+			metavar='A:B',
+			help='set histogram range'
+			)
+	parser.add_argument('-b', '--bins',
+			type=int,
+			help='set a number of bins'
+			)
+	parser.add_argument('--chan-col',
+			type=str, 
 			metavar = 'N',
-			help='an index of column with a channel name '
-				'(default: %d)' % CHAN_COL_IDX)
-
-	parser.add_argument('--data-col', type=str, 
+			default = CHAN_COL_IDX,
+			help='an index of column with a channel name'
+				';\n if None, all data belongs to the same channel'
+				' (default: %d)' % CHAN_COL_IDX
+			)
+	parser.add_argument('--data-col',
+			type=str, 
 			metavar = 'N',
-			help='an index of column with a data '
-				'(default: %d)' % DATA_COL_IDX)
-
-	parser.add_argument('-o','--output', type=str, default=None,
+			help='an index of column with a data'
+				' (default: %d)' % DATA_COL_IDX
+			)
+	parser.add_argument('-o','--output',
+			type=str,
 			metavar='PATH',
-			help="a path for output, one image per channel.")
+			help="a path for output, one file per channel."
+			)
+	parser.add_argument('-q','--quiet',
+			action='store_true',
+			help="minimize output to stderr"
+			)
 	
-	parser.add_argument('--debug', action='store_true',
-			help="be verbose")
+	parser.add_argument('--debug',
+			action='store_true',
+			help="be verbose"
+			)
+	parser.add_argument('--root-fit',
+			action='store_true',
+			help="instead of plotting fit the histograms with root_fit"
+			)
+	parser.add_argument('--root-gui',
+			action='store_true',
+			help="show ROOT graphical interface"
+			)
 	
 	args = parser.parse_args()
-	#~ print_err(args)
+	print_err(args)
 	
-	if args.data_col:
-		DATA_COL_IDX = args.data_col
-	if args.chan_col:
-		CHAN_COL_IDX = args.chan_col
-		
-	_range = map(float, args.range.split(':')[0:2]) #TODO: catch errors
-
-	#create out dir
-	prefix = args.output
-	if prefix:
-		folder = os.path.dirname(args.output)
-		if folder and not os.path.exists(folder):
-		    os.makedirs(folder)
+	# change default column indexes for channel names
+	CHAN_COL_IDX = args.chan_col  # can be None
 	
-	data={} # { channame: {filename: [values], ...}, ... }
-	
-	
-	# Parse the data
+	# parse the channels
 	chans = args.chan.split(',') if args.chan is not None else None
-	
-	print 'chans', chans
-	
-	for infile in args.infiles:
-		filename = infile.name
-		parsed = parse_input(infile, chans=chans) 
-
-		for channame, values in parsed.items():
-			if channame not in data:
-				data[channame] = {}
-				
-			data[channame][filename] = values
+	if not args.quiet:
+		print_err('chans: %s' %str(chans) )
 		
-			
-	opts = dict(
-		normed = args.normalize,
-		range=_range,
-		)
+	# create dir for output
+	outpath = makedirs(args.output)
 	
+	# parse range
+	_range = None
+	try:
+		if args.range:
+			_range = map(float, args.range.split(':')[:2])
+	
+	except ValueError:
+		print_err('wrong range value "%s" (should be two numbers, separated by ":")' % args.range)
+		exit(1)
+	
+	# get a number of bins
+	nbins = args.bins
+
+	# Parse the data
+	data = parse_infiles(args.infiles, chans=chans)  # { chan: [[values], ] ...}, ... }
+
+	# Set default values
+	for chan, val in data
+	
+	
+	if _range is None:
+		_range = auto_range()
+	
+	if bins is None:
+		bins = auto_bins()
+
+	histopts = dict(
+		normed=args.normalize,  # replace with density=True in future matplotlib versions 
+		range=_range,
+		alpha=0.75,
+		histtype='step',
+		)
+
+	chans = data.keys()  # channel names found
+	
+	exit(0)
+	
+	for chan in sorted(chans):  # TODO: numeric sort
+		pass
+	
+	
+	
+	# todo: for chan in ... 
+	# 		plot()
+	
+	# TODO: if --root-fit
+	# try to fit with root_fit instead of plotting
 	plot(data, bins = args.bins, outprefix = prefix, histopts=opts )
+	
+	
+	# TODO: range_min, range_max = percentile[5, 85] 
+	# range_min = 0 if range_min > 0 and range_min/range_max < 0.3
+	# auto_range
+	# auto_bins
+	#
 	
 	#~ print('Press Ctrl+C')
 	#~ signal.pause()
+
+
+def auto_range(data):
+	return (0,4000)
 	
+def auto_bins(datasets):
+	
+	dataiter = (d[:1000] for d in datasets)
+	
+	np.concatenate(dataiter)
+	return 100
+	
+
+
+def parse_infiles(infiles, chans=None):
+	"""
+	Call parse_infile for each infile and merge the data by channel name.
+	
+	Return dict of lists of lists:
+		{ channame: [[values], ...], ... }
+	"""
+	data = {}
+	for fd in infiles:
+		parsed = parse_infile(fd, chans=chans) 
+
+		for channame, values in parsed.items():
+			if channame not in data:
+				data[channame] = []
+				
+			data[channame].append(values)
+	
+	return data
+	
+	
+def makedirs(path):
+	""" Create directories for path (like `mkdir -p ...`). """
+	if not path:
+		return
+
+	folder = os.path.dirname(path)
+	if folder and not os.path.exists(folder):
+	    os.makedirs(folder)
+
+
+
 if __name__ == "__main__":
     main()
