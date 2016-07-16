577986af36c8ef75822ecb8464d0ba742d5c7979
Sergey Ryzhikov <sergey-inform@ya.ru>
2016-07-16 03:06:34 +0300

    temp

---
+++ root_fit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Fit data with a custom function.
+
+Author: Sergey Ryzhikov (sergey-inform@ya.ru), 2016
+License: GPLv2
+"""
+
+def root_fit(histdata, **params):
+	pass
+
+
+if __name__ == "__main__":
+	main()
