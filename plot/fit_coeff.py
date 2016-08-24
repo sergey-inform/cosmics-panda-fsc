717c7828ba73fa9c74840e159a558a53c7b7a056
Sergey Ryzhikov <Sergey.Ryzhikov@ihep.ru>
2016-08-24 14:24:01 +0300

    temp

---
+++ plot/fit_coeff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Read analysis results.
+# Calculate coefficients for each channel (mplA/mplB for trigA and trigB).
+
+
+def main():
+	#argparse filename and two trigger names
+	#read file data for this triggers
+	#calculate and print the values for each channel and run:
+	#  chan:  run1 run2 ...
+	
+ 
+
+
+
+if __name__ == "__main__":
+	main()
